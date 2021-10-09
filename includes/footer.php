8cf612c3644b0787eda9c95d92f28b005c12b5df
slickster23 <tokelo.foso23@gmail.com>
2021-10-09 13:02:13 +0200

    Commit

---
+++ includes/footer.php
@@ -0,0 +1,106 @@
+<footer class="page-footer">
+
+	<div class="footer-nav">
+		<div class="container clearfix">
+
+			<div class="footer-nav__col footer-nav__col--info">
+				<div class="footer-nav__heading">Information</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">The brand</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Local stores</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Customer service</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Privacy &amp; cookies</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Site map</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="footer-nav__col footer-nav__col--whybuy">
+				<div class="footer-nav__heading">Why buy from us</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Shipping &amp; returns</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Secure shipping</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Testimonials</a>
+					</li>
+					
+					
+				</ul>
+			</div>
+
+			
+
+			<div class="footer-nav__col footer-nav__col--contacts">
+				<div class="footer-nav__heading">Contact details</div>
+				<address class="address">
+				Head Office: ADAMBAKKAM STATIONERY.<br>
+				 Chennai, INDIA.
+			</address>
+				<div class="phone">
+					Telephone:
+					<a class="phone__number" href="tel:0123456789">0123-456-789</a>
+				</div>
+				<div class="email">
+					Email:
+					<a href="mailto:support@yourwebsite.com" class="email__addr">support@yourwebsite.com</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<!-- <div class="banners">
+		<div class="container clearfix">
+
+			<div class="banner-award">
+				<span>Award winner</span><br> Fashion awards 2016
+			</div>
+
+			<div class="banner-social">
+				<a href="#" class="banner-social__link">
+				<i class="icon-facebook"></i>
+			</a>
+				<a href="#" class="banner-social__link">
+				<i class="icon-twitter"></i>
+			</a>
+				<a href="#" class="banner-social__link">
+				<i class="icon-instagram"></i>
+			</a>
+				<a href="#" class="banner-social__link">
+				<i class="icon-pinterest-circled"></i>
+			</a>
+			</div>
+
+		</div>
+	</div> -->
+
+	<div class="page-footer__subline">
+		<div class="container clearfix">
+
+			<div class="copyright">
+				&copy; ADAMBAKKAM STATIONERY;
+			</div>
+
+			<div class="developer">
+				Dev by Code Breakers 2021 Hack4Equality
+			</div>
+
+			<div class="designby">
+				Design by Code Breakers 2021 Hack4Equality
+			</div>
+		</div>
+	</div>
+</footer>
